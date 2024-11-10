b57290b03359eed6ff415d1df3a15ab7ed4b2e25
Zyppon <mcpezyppon@gmail.com>
2024-11-10 04:59:22 +0100

    First Commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
+
+//export default {
+ //name: 'App',
+  //components: {
+    //HelloWorld
+  //}
+//}
+export default {
+  data() {
+    return {
+      message: ''
+    };
+  },
+  created() {
+    axios.get('http://localhost:8000/api/hello/')
+      .then(response => {
+        this.message = response.data.message;
+      })
+      .catch(error => {
+        console.error("There was an error!", error);
+      });
+  }
+};
+
+
+
+
+
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
